2faf5a837985d6b86c0656e9d764f5169bae21e4
timr <timrcode0@gmail.com>
2023-03-12 14:24:07 +0100

    add section 2

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
 
-// NAV BAR
-// Récupération de tous les liens de la barre de navigation
-const navLinks = document.querySelectorAll('nav ul li a');
-// Ajout d'un écouteur d'événement de défilement de page
-window.addEventListener('scroll', () => {
-  // Pour chaque section de la page
-    document.querySelectorAll('section').forEach(section => {
-        const sectionTop = section.offsetTop - 150; // Décalage de 50 pixels pour compenser la hauteur de la barre de navigation
-        const sectionHeight = section.offsetHeight;
+// Sélectionnez tous les liens dans la nav
+const links = document.querySelectorAll("header ul li a");
 
-        // Vérification si la section est visible à l'écran
-        if (window.pageYOffset >= sectionTop && window.pageYOffset < (sectionTop + sectionHeight)) {
-            const sectionId = section.getAttribute('id');
-
-        // Suppression de la classe active sur tous les liens de la barre de navigation
-            navLinks.forEach(link => link.classList.remove('active'));
-
-        // Ajout de la classe active sur le lien correspondant à la section en cours de visualisation
-            navLinks.forEach(link => {
-            if (link.getAttribute('href').slice(1) === sectionId) {
-            link.classList.add('active');
-            }
-        });
-        } else {
-        // Suppression de la classe active si la section n'est plus visible
-            const sectionId = section.getAttribute('id');
-            navLinks.forEach(link => {
-            if (link.getAttribute('href').slice(1) === sectionId) {
-                link.classList.remove('active');
-            }
-        });
-        }
+// Boucle à travers les liens et ajoute un écouteur d'événement
+links.forEach(link => {
+  link.addEventListener("mouseenter", () => {
+    // Change la couleur de texte pour le lien survolé
+    link.style.color = "white";
+    // Change la couleur de fond pour le lien survolé
+    // Boucle à travers les autres liens et change leur couleur de texte et de fond
+    links.forEach(otherLink => {
+      if (otherLink !== link) {
+        otherLink.style.color = "white";
+        otherLink.style.opacity ="0.5";
+  
+      }
     });
+  });
+  
+  link.addEventListener("mouseleave", () => {
+    // Change la couleur de texte et de fond pour tous les liens
+    links.forEach(otherLink => {
+      otherLink.style.color = "white";
+      otherLink.style.opacity ="1";
+    });
+  });
+
+  
+
 });
 
 
