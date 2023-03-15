59fe37ed8122da2c111a87bfa1884e762c099792
timr <timrcode0@gmail.com>
2023-03-15 19:43:54 +0100

    fix nav

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -31,7 +31,6 @@ links.forEach(link => {
 });
 
 const nav = document.querySelector('header nav');
-const header = document.querySelector('header')
 const before = document.querySelector(".background")
 
 console.log(before)
@@ -43,25 +42,58 @@ window.addEventListener("load", function() {
     nav.style.display="";
   }
 });
+let isNavFixed = false;
+
 // Ajouter un écouteur d'événement de défilement (scroll)
 window.addEventListener('scroll', () => {
   // Vérifiez si la position de défilement est supérieure à 20 pixels et inférieure à 500 pixels
   if (window.scrollY < 100) {
     // Ajouter la classe qui cache l'élément de navigation
     before.style.display="none";
-    nav.style.display="";
+    if (isNavFixed) {
+      // Restaurer la position relative de la barre de navigation
+      nav.style.top="";
+      isNavFixed = false;
+    }
     
-  } else if(window.scrollY < 600 ) {
+  } else if(window.scrollY < 500 ) {
     // Sinon, enlevez la classe qui cache l'élément de navigation
-    nav.style.display="none";;
     before.style.display="none";
-  }
-  else if(window.scrollY > 700) {
-    // Sinon, enlevez la classe qui cache l'élément de navigation
-    nav.style.display="sticky";
-    before.style.display="";
-  }
+    if (isNavFixed) {
+      // Restaurer la position relative de la barre de navigation
+      nav.style.position="absolute";
+      nav.style.top="";
+      isNavFixed = false;
+      
+    }
+
   
+  }
+  else if(window.scrollY > 600 && !isNavFixed) {
+    // Sinon, enlevez la classe qui cache l'élément de navigation
+    before.style.display="";
+    before.style.position="fixed";
+    // Animer la transition vers la position fixe
+    nav.style.position="fixed";
+    nav.animate([
+      { opacity: '0', transform: 'translateY(-50px)' },
+      { opacity: '1', transform: 'translateY(0)' }
+    ], {
+      duration: 500,
+      easing: 'ease-in-out'
+    });
+    
+    
+    before.animate([
+      { opacity: '0', transform: 'translateY(-100px)' },
+      { opacity: '1', transform: 'translateY(0px)' }
 
+
+    ], {
+      duration: 500,
+      easing: 'ease-in-out'
+    });
+    
+    isNavFixed = true;
+  }
 });
-
